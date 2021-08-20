01d1e847003b1d77f2eddb9e17a0f09fb83bd191
chris-luangrath <thechrisluangrath@gmail.com>
2021-08-20 14:01:40 -0600

    formatting

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,8 +1,9 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
-import React, {useCallback, useState} from "react";
-import {useMessaging} from "@footron/controls-client";
-import Button from '@material-ui/core/Button';
+import React, { useCallback, useState } from "react";
+import { useMessaging } from "@footron/controls-client";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core";
 
 // const containerStyle = css`
 //   padding: 16px;
@@ -13,22 +14,30 @@ import Button from '@material-ui/core/Button';
 //   }
 // `;
 
+const useButtonStyles = makeStyles({
+  contained: {
+    height: "400px",
+  },
+});
+
 const buttonStyle = css`
   #name {
     text-align: center;
     font-size: 40px;
   }
+
   #start {
     margin: auto;
   }
+
   .page-width-inner {
     // width: 100%;
     height: 100%;
     border: 1px solid black;
   }
+
   Button {
-    width:50%;
-    height:50%;
+    width: 50%;
     border: 1px solid black;
     color: whitesmoke;
     font-weight: bolder;
@@ -36,112 +45,153 @@ const buttonStyle = css`
     // background-color: var(--color);
     background-color: #6166ff;
   }
-  
-`
+
+  div {
+    height: 800%;
+  }
+
+  .css-ft6nkz-ControlsComponent {
+    height: 800%;
+  }
+
+  main {
+    height: 800%;
+  }
+`;
 
 const blue = css`
-  html{
+  html {
     --color: #6166ff;
   }
+
   Button {
     background-color: #6166ff;
   }
-`
+`;
 const green = css`
-  html{
+  html {
     --color: #3de364;
   }
+
   Button {
     background-color: #3de364;
   }
-`
+`;
 const red = css`
-  html{
+  html {
     --color: #ff6161;
   }
+
   Button {
     background-color: #ff6161;
   }
-`
+`;
 const yellow = css`
-  html{
+  html {
     --color: #fffc61;
   }
+
   Button {
     background-color: #fffc61;
   }
-`
+`;
 
 const ControlsComponent = (): JSX.Element => {
+  const [playerName, setPlayerName] = useState<string | undefined>();
 
-    const [playerName, setPlayerName] = useState<string | undefined>();
+  const { sendMessage } = useMessaging<{ player: string }>((message) => {
+    setPlayerName(message.player);
+  });
 
-    const { sendMessage } = useMessaging<{ player: string }>((message) => {
-        setPlayerName(message.player)
-    });
+  const update = useCallback(
+    async (movement) => {
+      if (!playerName) return;
+      await sendMessage({ player: playerName, movement: movement });
+    },
+    [sendMessage, playerName]
+  );
+  const buttonClasses = useButtonStyles();
 
-    const update = useCallback(
-        async (movement) => {
-            if (!playerName) return;
-            await sendMessage({player: playerName, movement: movement});
-        },
-        [sendMessage, playerName]
-    );
+  function up() {
+    update(0);
+  }
 
-    function up() {
-        update(0);
+  function down() {
+    update(2);
+  }
+
+  function stop() {
+    update(1);
+  }
+
+  function start() {
+    update(3);
+  }
+
+  function stap() {
+    // for testing purposes
+    return 1 + 1;
+  }
+
+  // let PlayerName = ({
+  //     // eslint-disable-next-line react/display-name
+  //     render: function() {
+  //         return <div> {player}! </div>;
+  //     }
+  // })
+
+  let color = css`
+    html {
     }
+  `;
 
-    function down() {
-        update(2);
-    }
+  if (playerName === "left") {
+    color = blue;
+  } else if (playerName === "right") {
+    color = green;
+  } else if (playerName === "up") {
+    color = red;
+  } else if (playerName === "down") {
+    color = yellow;
+  }
+  return (
+    <div css={buttonStyle}>
+      <div id={"name"}>{playerName || "unknown"}</div>
+      <Button type="button" id={"start"} onMouseUp={start}>
+        Start
+      </Button>
 
-    function stop() {
-        update(1);
-    }
+      <div>
 
-    function start() {
-        update(3)
-    }
+        <Button
+          type="button"
+          variant={"contained"}
+          id={"right"}
+          size={"large"}
+          onTouchStart={down}
+          onTouchEnd={stop}
+          classes={{
+            contained: buttonClasses.contained,
+          }}
+        >
+          Down
+        </Button>
+        <Button
+            type="button"
+            variant={"contained"}
+            id={"left"}
+            onTouchStart={up}
+            onTouchEnd={stop}
+            classes={{
+              contained: buttonClasses.contained,
+            }}
+        >
+          Up
+        </Button>
+      </div>
 
-    function stap(){
-        // for testing purposes
-        return 1 + 1;
-    }
-
-    // let PlayerName = ({
-    //     // eslint-disable-next-line react/display-name
-    //     render: function() {
-    //         return <div> {player}! </div>;
-    //     }
-    // })
-
-    let color = css`
-        html {
-          
-        }
-    `
-
-    if(playerName === "left"){
-        color = blue;
-    } else if(playerName === "right"){
-        color = green;
-    } else if(playerName === "up"){
-        color = red;
-    } else if(playerName === "down"){
-        color = yellow;
-    }
-        return (
-        <div css={buttonStyle}>
-            <div id={"name"}>{playerName || "unknown"}</div>
-            <Button type="button" id={"start"} onMouseUp={start}>Start</Button>
-            <div>
-                <Button type="button" id={"left"} size={"large"} onTouchStart={up} onTouchEnd={stop} >Up</Button>
-                <Button type="button" id={"right"} size={"large"}  onTouchStart={down} onTouchEnd={stop} >Down</Button>
-            </div>
-        </div>
-
-    );
+    </div>
+  );
 };
 
 export default ControlsComponent;
